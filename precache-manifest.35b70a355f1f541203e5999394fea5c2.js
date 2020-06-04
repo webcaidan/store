1f270e7aed5f1d7f831b3a0c3119b015f09ad3b6
webcaidan <a15zehcheche@iam.cat>
2020-06-04 12:03:46 +0200

    Updates

---
+++ precache-manifest.35b70a355f1f541203e5999394fea5c2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e54c3c716c67dedd04218ef2802f9800",
+    "url": "/store/index.html"
+  },
+  {
+    "revision": "4c4a7c943fb2653745f4",
+    "url": "/store/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "51f46168cbe515fc623d",
+    "url": "/store/static/css/main.c1459032.chunk.css"
+  },
+  {
+    "revision": "4c4a7c943fb2653745f4",
+    "url": "/store/static/js/2.cb7f9326.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/store/static/js/2.cb7f9326.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51f46168cbe515fc623d",
+    "url": "/store/static/js/main.93e12ca9.chunk.js"
+  },
+  {
+    "revision": "5ae31ff311ca5f676c2e",
+    "url": "/store/static/js/runtime-main.da3bb32f.js"
+  },
+  {
+    "revision": "fefc80525d8619e37030bc346c39df86",
+    "url": "/store/static/media/Brandhill-Web-Background.fefc8052.jpg"
+  },
+  {
+    "revision": "fd4cdde060b9039849a5cd62e06199ad",
+    "url": "/store/static/media/carousel-bg.fd4cdde0.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/store/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2d1d1fe0fbd568a44c403e952b1e2004",
+    "url": "/store/static/media/menu1.2d1d1fe0.png"
+  },
+  {
+    "revision": "0589c0d40f09881984afdcc894f6a8a2",
+    "url": "/store/static/media/menu2.0589c0d4.png"
+  },
+  {
+    "revision": "63aa8289588635ed98be3b86d9f02e0f",
+    "url": "/store/static/media/menu3.63aa8289.png"
+  }
+]);
